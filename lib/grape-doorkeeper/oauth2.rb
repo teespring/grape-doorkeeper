7a3f2f7d50ddffbb7eca428bfd6439c0c7905f77
Stan Bondi <stan@fixate.it>
2015-02-10 13:37:28 +0200

    White space cleanup

--- lib/grape-doorkeeper/oauth2.rb
+++ lib/grape-doorkeeper/oauth2.rb
@@ -1,30 +1,31 @@
 require 'grape/api'
 require 'doorkeeper/doorkeeper_for'
+
 module GrapeDoorkeeper
   # OAuth 2.0 authorization for Grape APIs.
-  
+
   class Middleware < GrapeDoorkeeper::OAuth2Base
-  
+
     def protected_endpoint?
       endpoint = env['api.endpoint']
       return endpoint.options[:route_options][:protected] if endpoint.options[:route_options].key?(:protected)
-    
+
       filter_options = options[:doorkeeper].filter_options
       return true if filter_options.blank? #protect all routes
-                  
+
       action = endpoint.namespace.split('/')[1] || endpoint.options[:path].first
-      
+
       if filter_options[:only]
         return filter_options[:only].include?( action.to_sym )
       elsif filter_options[:except]
-        return !filter_options[:except].include?( action.to_sym )     
+        return !filter_options[:except].include?( action.to_sym )
       end
-      
-      false      
+
+      false
     end
-  
+
     def verify_token(token_string)
-      return unless protected_endpoint?      
+      return unless protected_endpoint?
       token = Doorkeeper::AccessToken.authenticate(token_string)
       doorkeeper = options[:doorkeeper]
       if env['api.endpoint'].options[:route_options].key?(:scopes)
@@ -34,7 +35,7 @@ module GrapeDoorkeeper
       if token
         if !token.accessible?
           error_out(401, 'expired_token')
-        else 
+        else
           if token.includes_scope?(doorkeeper.scopes)
             env['api.token'] = token
           else
@@ -45,10 +46,10 @@ module GrapeDoorkeeper
         error_out(401, 'invalid_token')
       end
     end
-    
-    def error_out(status, error)    
-      scopes = options[:doorkeeper].instance_variable_get(:@scopes)    
-           
+
+    def error_out(status, error)
+      scopes = options[:doorkeeper].instance_variable_get(:@scopes)
+
       throw :error,
             message:  {error: error},
             status: status,
@@ -58,16 +59,16 @@ module GrapeDoorkeeper
               'WWW-Authenticate' => "OAuth realm='#{options[:realm]}', error='#{error}'"
             }.reject { |k,v| v.nil? }
     end
-    
+
   end
-  
+
   module OAuth2
     def doorkeeper_for *args
       doorkeeper_for = Doorkeeper::DoorkeeperForBuilder.create_doorkeeper_for(*args)
       use GrapeDoorkeeper::Middleware, doorkeeper: doorkeeper_for
     end
   end
-    
+
 end
 
 Grape::API.extend GrapeDoorkeeper::OAuth2
